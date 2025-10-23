49e63a63850907b78284aef81568f5a3255847f8
khat <khaquachtrong74@gmail.com>
2025-10-23 08:56:38 +0700

    deleted frontend

--- frontend/src/services/index.ts
+++
@@ -1,7 +0,0 @@
-// Central export cho tất cả services
-// Import từ đây thay vì import trực tiếp từ từng file
-
-export { apiService } from './api';
-export { authService } from './auth';
-export { reportsService } from './reports';
-export { usersService } from './users';
